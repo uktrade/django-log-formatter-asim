5143ccac56cb856db90ee69cf6f054a1352c0706
Will Gibson <8738245+WillGibson@users.noreply.github.com>
2023-11-16 03:21:41 -0800

    DBTP-570 Initial "rip off" of `django-log-formatter-ecs` (#1)
    Co-authored-by: Yusuf <47318342+yusufsheiqh@users.noreply.github.com>

---
+++ django_log_formatter_asim/__init__.py
@@ -0,0 +1,218 @@
+import json
+import logging
+import os
+import platform
+from urllib.parse import urlparse
+
+from django.conf import settings
+# TODO...
+from kubi_ecs_logger import Logger
+from kubi_ecs_logger.models import BaseSchema
+from kubi_ecs_logger.models import Severity
+
+# TODO: Event categories - https://www.elastic.co/guide/en/ecs/current/ecs-allowed-values-event-category.html  # noqa E501
+CATEGORY_DATABASE = "database"
+CATEGORY_PROCESS = "process"
+CATEGORY_WEB = "web"
+
+
+class ASIMLogger(Logger):
+    def __init__(self, *args, **kwargs):
+        super(ASIMLogger, self).__init__(*args, **kwargs)
+
+    def get_log_dict(self):
+        return BaseSchema().dump(self._base)
+
+
+class ASIMFormatterBase:
+    def __init__(self, record):
+        self.record = record
+
+    def _get_event_base(self, extra_labels={}):
+        labels = {
+            "application": getattr(settings, "DLFE_APP_NAME", None),
+            "env": self._get_environment(),
+        }
+
+        logger = (
+            ASIMLogger()
+            .event(
+                category=self._get_event_category(),
+                action=self.record.name,
+                message=self.record.getMessage(),
+                labels={
+                    **labels,
+                    **extra_labels,
+                },
+            )
+            .host(
+                architecture=platform.machine(),
+            )
+        )
+
+        return logger
+
+    def _get_event_category(self):
+        if self.record.name in ("django.request", "django.server"):
+            return CATEGORY_WEB
+        if self.record.name.startswith("django.db.backends"):
+            return CATEGORY_DATABASE
+
+        return CATEGORY_PROCESS
+
+    def _get_environment(self):
+        return os.getenv("DJANGO_SETTINGS_MODULE") or "Unknown"
+
+
+class ASIMSystemFormatter(ASIMFormatterBase):
+    def get_event(self):
+        logger_event = self._get_event_base()
+
+        return logger_event
+
+
+class ASIMDBFormatter(ASIMFormatterBase):
+    # created for augmentation based on django.db.backends
+    def get_event(self):
+        logger_event = self._get_event_base()
+
+        return logger_event
+
+
+class ASIMRequestFormatter(ASIMFormatterBase):
+    def get_event(self):
+        zipkin_headers = getattr(
+            settings,
+            "DLFE_ZIPKIN_HEADERS",
+            ("X-B3-TraceId", "X-B3-SpanId"),
+        )
+
+        extra_labels = {}
+
+        for zipkin_header in zipkin_headers:
+            if getattr(
+                self.record.request.headers,
+                zipkin_header,
+                None,
+            ):
+                extra_labels[zipkin_header] = self.record.request.headers[
+                    zipkin_header
+                ]  # noqa E501
+
+        logger_event = self._get_event_base(
+            extra_labels=extra_labels,
+        )
+
+        parsed_url = urlparse(self.record.request.build_absolute_uri())
+
+        ip = self._get_ip_address(self.record.request)
+
+        request_bytes = len(self.record.request.body)
+
+        logger_event.url(
+            path=parsed_url.path,
+            domain=parsed_url.hostname,
+        ).source(
+            ip=self._get_ip_address(self.record.request)
+        ).http_response(status_code=getattr(self.record, "status_code", None)).client(
+            address=ip,
+            bytes=request_bytes,
+            domain=parsed_url.hostname,
+            ip=ip,
+            port=parsed_url.port,
+        ).http_request(
+            body_bytes=request_bytes,
+            body_content=self.record.request.body,
+            method=self.record.request.method,
+        )
+
+        user_agent_string = getattr(
+            self.record.request.headers,
+            "user_agent",
+            None,
+        )
+
+        if not user_agent_string and "HTTP_USER_AGENT" in self.record.request.META:  # noqa E501
+            user_agent_string = self.record.request.META["HTTP_USER_AGENT"]
+
+        # Check for use of django-user_agents
+        if getattr(self.record.request, "user_agent", None):
+            logger_event.user_agent(
+                device={
+                    "name": self.record.request.user_agent.device.family,
+                },
+                name=self.record.request.user_agent.browser.family,
+                original=user_agent_string,
+                version=self.record.request.user_agent.browser.version_string,
+            )
+        elif user_agent_string:
+            logger_event.user_agent(
+                original=user_agent_string,
+            )
+
+        if getattr(self.record.request, "user", None):
+            if getattr(settings, "DLFE_LOG_SENSITIVE_USER_DATA", False):
+                # Defensively check for full name due to possibility of custom user app
+                try:
+                    full_name = self.record.request.user.get_full_name()
+                except AttributeError:
+                    full_name = None
+
+                # Check user attrs to account for custom user apps
+                logger_event.user(
+                    email=getattr(self.record.request.user, "email", None),
+                    full_name=full_name,
+                    name=getattr(self.record.request.user, "username", None),
+                    id=getattr(self.record.request.user, "id", None),
+                )
+            else:
+                logger_event.user(
+                    id=getattr(self.record.request.user, "id", None),
+                )
+
+        return logger_event
+
+    def _get_ip_address(self, request):
+        # Import here as ipware uses settings
+        from ipware import get_client_ip
+
+        client_ip, is_routable = get_client_ip(request)
+        return client_ip or "Unknown"
+
+
+ASIM_FORMATTERS = {
+    "root": ASIMSystemFormatter,
+    "django.request": ASIMRequestFormatter,
+    "django.db.backends": ASIMSystemFormatter,
+}
+
+
+class ASIMFormatter(logging.Formatter):
+    def format(self, record):
+        if record.name in ASIM_FORMATTERS:
+            asim_formatter = ASIM_FORMATTERS[record.name]
+        else:
+            asim_formatter = ASIMSystemFormatter
+
+        formatter = asim_formatter(record=record)
+        logger_event = formatter.get_event()
+
+        logger_event.log(
+            level=self._get_severity(record.levelname),
+        )
+
+        log_dict = logger_event.get_log_dict()
+
+        return json.dumps(log_dict)
+
+    def _get_severity(self, level):
+        if level == "DEBUG":
+            return Severity.DEBUG
+        elif level == "INFO":
+            return Severity.INFO
+        elif level == "WARNING":
+            return Severity.WARNING
+        elif level == "ERROR":
+            return Severity.ERROR
+        elif level == "CRITICAL":
+            return Severity.CRITICAL
---
+++ tests/test_django_log_formatter.py
@@ -0,0 +1,116 @@
+import json
+import logging
+import os
+from io import StringIO
+from unittest import TestCase
+from unittest.mock import patch
+
+from django.conf import settings
+from django.test import RequestFactory
+from django.test import override_settings
+
+from django_log_formatter_asim import ASIMFormatter
+
+settings.configure(
+    DEBUG=True,
+    ALLOWED_HOSTS="*",
+)
+
+
+class User:
+    def __init__(self, email, user_id, first_name, last_name, username):
+        self.email = email
+        self.id = user_id
+        self.first_name = first_name
+        self.last_name = last_name
+        self.username = username
+
+    def get_full_name(self):
+        return f"{self.first_name} {self.last_name}"
+
+
+class ASIMFormatterTest(TestCase):
+    def setUp(self):
+        self.factory = RequestFactory()
+
+    def create_logger(self, logger_name):
+        log_buffer = StringIO()
+        asim_handler = logging.StreamHandler(log_buffer)
+        asim_handler.setFormatter(ASIMFormatter())
+
+        logger = logging.getLogger(logger_name)
+        logger.addHandler(asim_handler)
+        logger.setLevel(logging.DEBUG)
+        logging.propagate = False
+
+        return logger, log_buffer
+
+    def test_record_base_formatting(self):
+        logger, log_buffer = self.create_logger("django")
+        logger.debug("Test")
+        json_output = log_buffer.getvalue()
+        output = json.loads(json_output)
+
+        assert output["event"]["message"] == "Test"
+
+    def _create_request_log(self, add_user=False):
+        request = self.factory.get("/")
+
+        if add_user:
+            user = User(
+                email="test@test.com",
+                user_id=1,
+                first_name="John",
+                last_name="Test",
+                username="johntest",
+            )
+            setattr(request, "user", user)
+
+        logger, log_buffer = self.create_logger("django.request")
+        logger.error(
+            msg="Request test",
+            extra={
+                "request": request,
+            },
+        )
+
+        json_output = log_buffer.getvalue()
+
+        return json.loads(json_output)
+
+    def test_request_formatting(self):
+        output = self._create_request_log()
+
+        assert output["event"]["message"] == "Request test"
+
+    def test_log_sensitive_user_data_default(self):
+        output = self._create_request_log(add_user=True)
+
+        assert "id" in output["user"]
+        assert "email" not in output["user"]
+
+    @override_settings(DLFE_LOG_SENSITIVE_USER_DATA=True)
+    def test_log_sensitive_user_data_on(self):
+        output = self._create_request_log(add_user=True)
+
+        assert output["user"]["id"] == "1"
+        assert output["user"]["email"] == "test@test.com"
+        assert output["user"]["full_name"] == "John Test"
+        assert output["user"]["name"] == "johntest"
+
+    @override_settings(DLFE_APP_NAME="TestApp")
+    def test_app_name_log_value(self):
+        output = self._create_request_log()
+
+        assert output["event"]["labels"]["application"] == "TestApp"
+
+    def test_env_unset_log_value(self):
+        output = self._create_request_log()
+
+        assert output["event"]["labels"]["env"] == "Unknown"
+
+    @patch.dict(os.environ, {"DJANGO_SETTINGS_MODULE": "settings.Test"})
+    def test_env_log_value(self):
+        output = self._create_request_log()
+
+        assert output["event"]["labels"]["env"] == "settings.Test"
