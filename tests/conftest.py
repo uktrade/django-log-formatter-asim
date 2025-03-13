2298588535cdcc5b3cd0173cd2a83768ff3da9f3
Aadam Ali <57071686+Aadam-Ali@users.noreply.github.com>
2025-03-13 14:04:56 +0000

    tests: datadog trace dict when env vars not set
    Added a test which checks that the `env`, `service`, and `version`
    fields are empty when the `DD_*` environment variables are not set.
    
    This also moves the logic to set the `DD_*` environment variables and
    reload `ddtrace` into the test that requires it only from the
    `conftest.py` file. The test also unsets the environment variables, and
    again reloads `ddtrace` to avoid test pollution.
    
    <https://uktrade.atlassian.net/browse/DBTP-1662>

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,18 +1,12 @@
-from importlib import reload
 import os
 
-import ddtrace
 import django
 
 
 def pytest_configure(config):
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "tests.settings")
-    os.environ["DD_ENV"] = "test"
-    os.environ["DD_SERVICE"] = "django-service"
-    os.environ["DD_VERSION"] = "1.0.0"
     os.environ["ECS_CONTAINER_METADATA_URI"] = (
         "http://169.254.170.2/v3/709d1c10779d47b2a84db9eef2ebd041-0265927825"
     )
-    reload(ddtrace)
 
     django.setup()
