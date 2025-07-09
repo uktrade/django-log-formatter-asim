66ceb51113ddde6bea555e2977bbd8611d6c039f
Adrian Clay <adrian.clay@digital.trade.gov.uk>
2025-07-09 12:59:51 +0100

    feat: Create method for logging Cyber Authentication events (DBTP-2167) (#33)
    This can be used by downstream applications to simplify cyber
    logging requirements.

---
+++ django_log_formatter_asim/events.py
@@ -0,0 +1,212 @@
+import datetime
+import json
+from enum import Enum
+from typing import Literal
+from typing import Optional
+from typing import TypedDict
+
+from django.http import HttpRequest
+
+
+class AuthenticationEvent(str, Enum):
+    Logon = "Logon"
+    Logoff = "Logoff"
+
+
+class AuthenticationResult(str, Enum):
+    Success = "Success"
+    Partial = "Partial"
+    Failure = "Failure"
+    NA = "NA"
+
+
+class AuthenticationLoginMethod(str, Enum):
+    UsernamePassword = "Username & Password"
+    StaffSSO = "Staff-SSO"
+    UKGOVSSO = "UK.GOV-SSO"
+    ExternalIDP = "External IdP"
+
+
+class Severity(str, Enum):
+    Informational = "Informational"
+    Low = "Low"
+    Medium = "Medium"
+    High = "High"
+
+
+class AuthenticationServer(TypedDict):
+    """Dictionary to represent properties of the HTTP Server."""
+
+    """
+    A unique identifier for the server which serviced the Authentication event.
+
+    Defaults to the WSGI SERVER_NAME field if not provided.
+    """
+    hostname: Optional[str]
+    """Internet Protocol Address of the server serving this request."""
+    ip_address: Optional[str]
+
+
+class AuthenticationClient(TypedDict):
+    """Dictionary to represent properties of the HTTP Client."""
+
+    """Internet Protocol Address of the client making the Authentication
+    event."""
+    ip_address: Optional[str]
+
+
+class AuthenticationUser(TypedDict):
+    """Dictionary to represent properties of the users session."""
+
+    """What type of role best describes this Authentication event."""
+    role: Optional[
+        Literal[
+            "Regular",
+            "Machine",
+            "Admin",
+            "System",
+            "Application",
+            "Service Principal",
+            "Service",
+            "Anonymous",
+            "Other",
+        ]
+    ]
+    """
+    A unique identifier for the user.
+
+    Defaults to the logged in Django User.username if not provided.
+    """
+    username: Optional[str]
+    """
+    A unique identifier for this authentication session if one exists.
+
+    Defaults to the Django Sessions session key if not provided.
+    """
+    sessionId: Optional[str]
+
+
+def log_authentication(
+    request: HttpRequest,
+    event: AuthenticationEvent,
+    result: AuthenticationResult,
+    login_method: AuthenticationLoginMethod,
+    user: Optional[AuthenticationUser] = None,
+    server: Optional[AuthenticationServer] = None,
+    client: Optional[AuthenticationClient] = None,
+    severity: Optional[Severity] = None,
+    time_generated: Optional[datetime.datetime] = None,
+    result_details: Optional[str] = None,
+    message: Optional[str] = None,
+):
+    """
+    Log an ASIM Authentication Event to standard output.
+
+    :param request: django.http.HttpRequest object which initiated this Authentication request
+                    from which the following data will be logged if available
+                        - Django Authentication systems current username
+                        - Django Session middlewares Session Key
+                        - Client IP address
+                        - Server hostname
+    :param event: What authentication action was attempted, either "Logon" or "Logoff"
+    :param result: What outcome did the action have, either "Success", "Failure", "Partial", "NA"
+    :param login_method: What authentication mechanism was being used, one of:
+                        - "Username & Password"
+                        - "Staff-SSO"
+                        - "UK.GOV-SSO"
+                        - "External IdP"
+    :param user: Dictionary containing information on the subject of this Authentication event
+                 see AuthenticationUser class for more details.
+    :param server: Dictionary containing information on the server servicing this Authentication event
+                   see AuthenticationServer class for more details.
+    :param client: Dictionary containing information on the client performing this Authentication event
+                   see AuthenticationClient class for more details.
+    :param severity: Optional severity of the event, defaults to "Informational", otherwise one of:
+                        - "Informational"
+                        - "Low"
+                        - "Medium"
+                        - "High"
+    :param time_generated: Optional datetime for when the event happened, otherwise datetime.now
+    :param result_details: Optional string describing any details associated with the events outcome.
+                           This field is typically populated when the result is a failure.
+    :param message: Optional string describing the reason why the log was generated.
+
+    See also: https://learn.microsoft.com/en-us/azure/sentinel/normalization-schema-authentication
+    """
+    if user == None:
+        user = {}
+    if server == None:
+        server = {}
+    if client == None:
+        client = {}
+
+    event_created = time_generated or datetime.datetime.now(tz=datetime.timezone.utc)
+
+    log = {
+        "EventCreated": event_created.isoformat(),  # TODO: Should this really be EventCreated, or TimeGenerated
+        "EventSeverity": severity or _default_severity(result),
+        "EventOriginalType": _event_code(event, result),
+        "EventType": event,
+        "EventResult": result,
+        "LogonMethod": login_method,
+        "EventSchema": "Authentication",
+        "EventSchemaVersion": "0.1.4",
+    }
+
+    if "hostname" in server:
+        log["DvcHostname"] = server["hostname"]
+    elif hasattr(request, "environ") and "SERVER_NAME" in request.environ:
+        log["DvcHostname"] = request.environ["SERVER_NAME"]
+
+    if "ip_address" in client:
+        log["SrcIpAddr"] = client["ip_address"]
+    elif hasattr(request, "environ") and "REMOTE_ADDR" in request.environ:
+        log["SrcIpAddr"] = request.environ.get("REMOTE_ADDR")
+
+    if "role" in user:
+        log["ActorUserType"] = user["role"]
+
+    if "sessionId" in user:
+        log["ActorSessionId"] = user["sessionId"]
+    elif request.session.session_key:
+        log["ActorSessionId"] = request.session.session_key
+
+    if "username" in user:
+        log["ActorUsername"] = user["username"]
+    elif request.user.username:
+        log["ActorUsername"] = request.user.username
+
+    if result_details:
+        log["EventResultDetails"] = result_details
+
+    if message:
+        log["EventMessage"] = message
+
+    if "ip_address" in server:
+        log["DvcIpAddr"] = server["ip_address"]
+
+    print(json.dumps(log), flush=True)
+
+
+log_authentication.Event = AuthenticationEvent
+log_authentication.Result = AuthenticationResult
+log_authentication.LoginMethod = AuthenticationLoginMethod
+log_authentication.Severity = Severity
+
+
+def _default_severity(result: AuthenticationResult) -> Severity:
+    return Severity.Informational if result == AuthenticationResult.Success else Severity.Medium
+
+
+def _event_code(event: AuthenticationEvent, result: AuthenticationResult) -> str:
+    if event == AuthenticationEvent.Logon:
+        if result == log_authentication.Result.Success:
+            return "001a"
+        elif result == AuthenticationResult.Failure:
+            return "001b"
+    elif event == AuthenticationEvent.Logoff:
+        if result == AuthenticationResult.Success:
+            return "001c"
+        elif result == AuthenticationResult.Failure:
+            return "001d"
+    return "001"
---
+++ tests/test_log_authentication.py
@@ -0,0 +1,202 @@
+import datetime
+import json
+from collections import namedtuple
+
+import pytest
+from freezegun import freeze_time
+
+from django_log_formatter_asim.events import log_authentication
+
+
+@pytest.fixture()
+def wsgi_request():
+    return namedtuple("Request", ["environ", "user", "session"])(
+        {"REMOTE_ADDR": "192.168.1.101", "SERVER_NAME": "WebServer.local"},
+        namedtuple("User", ["username"])("Adrian"),
+        namedtuple("Session", ["session_key"])("def456"),
+    )
+
+
+class TestLogAuthentication:
+    def test_authentication_specifying_all_fields(self, wsgi_request, capsys):
+        log_authentication(
+            wsgi_request,
+            event=log_authentication.Event.Logon,
+            result=log_authentication.Result.Failure,
+            login_method=log_authentication.LoginMethod.UsernamePassword,
+            user={
+                "username": "Billy-the-fish",
+                "role": "Administrator",
+                "sessionId": "abc123",
+            },
+            server={"hostname": "BigServer", "ip_address": "127.0.0.1"},
+            client={"ip_address": "192.168.1.100"},
+            severity=log_authentication.Severity.Low,
+            time_generated=datetime.datetime(2025, 1, 2, 3, 4, 5, tzinfo=datetime.timezone.utc),
+            result_details="Biometric and SmartCard authentication",
+            message="Billy tried real hard to get in, but his fishy features werent recognised",
+        )
+
+        structured_log_entry = self._get_structured_log_entry(capsys)
+
+        # ASIM CommonFields
+        assert structured_log_entry["EventSchema"] == "Authentication"
+        assert structured_log_entry["EventSchemaVersion"] == "0.1.4"
+        assert structured_log_entry["EventType"] == "Logon"
+        assert structured_log_entry["EventResult"] == "Failure"
+        assert structured_log_entry["EventCreated"] == "2025-01-02T03:04:05+00:00"
+        assert structured_log_entry["DvcHostname"] == "BigServer"
+        assert structured_log_entry["DvcIpAddr"] == "127.0.0.1"
+        assert structured_log_entry["EventSeverity"] == "Low"
+        assert (
+            structured_log_entry["EventMessage"]
+            == "Billy tried real hard to get in, but his fishy features werent recognised"
+        )
+        assert structured_log_entry["SrcIpAddr"] == "192.168.1.100"
+        assert (
+            structured_log_entry["EventResultDetails"] == "Biometric and SmartCard authentication"
+        )
+
+        # ASIM Authentication Specific Fields
+        assert structured_log_entry["LogonMethod"] == "Username & Password"
+        assert structured_log_entry["ActorUsername"] == "Billy-the-fish"
+        assert structured_log_entry["ActorSessionId"] == "abc123"
+        assert structured_log_entry["ActorUserType"] == "Administrator"
+
+    @pytest.mark.parametrize(
+        "event, event_result, expected_event_code",
+        [
+            ("Logon", "Success", "001a"),
+            ("Logon", "Failure", "001b"),
+            ("Logoff", "Success", "001c"),
+            ("Logoff", "Failure", "001d"),
+            ("Logon", "Partial", "001"),
+            ("Logoff", "Partial", "001"),
+            ("Invalid", "Invalid", "001"),
+        ],
+    )
+    def test_authentication_event_codes(
+        self, wsgi_request, capsys, event, event_result, expected_event_code
+    ):
+        log_authentication(
+            wsgi_request,
+            event=event,
+            result=event_result,
+            login_method=log_authentication.LoginMethod.UsernamePassword,
+        )
+
+        structured_log_entry = self._get_structured_log_entry(capsys)
+
+        assert structured_log_entry["EventOriginalType"] == expected_event_code
+
+    @freeze_time("2025-07-02 08:15:20")
+    def test_authentication_populates_logs_from_current_time_and_request_varaible(
+        self, wsgi_request, capsys
+    ):
+        log_authentication(
+            wsgi_request,
+            event=log_authentication.Event.Logoff,
+            result=log_authentication.Result.Success,
+            login_method=log_authentication.LoginMethod.UsernamePassword,
+        )
+
+        structured_log_entry = self._get_structured_log_entry(capsys)
+
+        assert structured_log_entry["EventCreated"] == "2025-07-02T08:15:20+00:00"
+        assert structured_log_entry["DvcHostname"] == "WebServer.local"
+        assert structured_log_entry["SrcIpAddr"] == "192.168.1.101"
+        assert structured_log_entry["ActorUsername"] == "Adrian"
+        assert structured_log_entry["ActorSessionId"] == "def456"
+
+    @pytest.mark.parametrize(
+        "event_result, expected_event_severity",
+        [
+            ("Success", "Informational"),
+            ("Failure", "Medium"),
+            ("Partial", "Medium"),
+            ("NA", "Medium"),
+        ],
+    )
+    def test_authentication_sets_event_severity_based_on_result(
+        self, wsgi_request, capsys, event_result, expected_event_severity
+    ):
+        log_authentication(
+            wsgi_request,
+            event=log_authentication.Event.Logoff,
+            result=event_result,
+            login_method=log_authentication.LoginMethod.UsernamePassword,
+        )
+
+        structured_log_entry = self._get_structured_log_entry(capsys)
+
+        assert structured_log_entry["EventSeverity"] == expected_event_severity
+
+    def test_authentication_does_not_populate_fields_which_are_not_provided(self, capsys):
+        wsgi_request = namedtuple("Request", ["environ", "user", "session"])(
+            {"REMOTE_ADDR": "192.168.1.101", "SERVER_NAME": "WebServer.local"},
+            namedtuple("User", ["username"])(None),
+            namedtuple("Session", ["session_key"])(None),
+        )
+
+        log_authentication(
+            wsgi_request,
+            event=log_authentication.Event.Logoff,
+            result=log_authentication.Result.Success,
+            login_method=log_authentication.LoginMethod.UsernamePassword,
+        )
+
+        structured_log_entry = self._get_structured_log_entry(capsys)
+
+        assert "ActorUserType" not in structured_log_entry
+        assert "ActorSessionId" not in structured_log_entry
+        assert "ActorUsername" not in structured_log_entry
+        assert "DvcIpAddr" not in structured_log_entry
+        assert "EventMessage" not in structured_log_entry
+        assert "EventResultDetails" not in structured_log_entry
+
+    def test_authentication_does_not_populate_request_environ_fields_when_environ_is_not_provided(
+        self, capsys
+    ):
+        wsgi_request = namedtuple("Request", ["user", "session"])(
+            namedtuple("User", ["username"])(None),
+            namedtuple("Session", ["session_key"])(None),
+        )
+
+        log_authentication(
+            wsgi_request,
+            event=log_authentication.Event.Logoff,
+            result=log_authentication.Result.Success,
+            login_method=log_authentication.LoginMethod.UsernamePassword,
+        )
+
+        structured_log_entry = self._get_structured_log_entry(capsys)
+
+        assert "DvcHostname" not in structured_log_entry
+        assert "SrcIpAddr" not in structured_log_entry
+
+    def test_authentication_populates_fields_which_are_provided_as_none(self, wsgi_request, capsys):
+        log_authentication(
+            wsgi_request,
+            event=log_authentication.Event.Logoff,
+            result=log_authentication.Result.Success,
+            login_method=log_authentication.LoginMethod.UsernamePassword,
+            user={"role": None, "sessionId": None, "username": None},
+            server={"hostname": None},
+            client={"ip_address": None},
+        )
+
+        structured_log_entry = self._get_structured_log_entry(capsys)
+
+        assert structured_log_entry["ActorUserType"] is None
+        assert structured_log_entry["ActorSessionId"] is None
+        assert structured_log_entry["ActorUsername"] is None
+        assert structured_log_entry["DvcHostname"] is None
+        assert structured_log_entry["SrcIpAddr"] is None
+
+    def _get_structured_log_entry(self, capsys):
+        (out, _) = capsys.readouterr()
+        self._assert_has_one_new_line_at_end_of_string(out)
+        return json.loads(out)
+
+    def _assert_has_one_new_line_at_end_of_string(self, expected):
+        assert expected.find("\n") == len(expected) - 1
