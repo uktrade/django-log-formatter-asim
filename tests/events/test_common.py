5f71ae970c75f3229b610a013bc330f803598d77
Adrian Clay <adrian.clay@digital.trade.gov.uk>
2025-08-01 13:25:37 +0100

    refactor: Extract common event fields behaviour (off-ticket) (#57)

--- tests/events/test_common.py
+++ tests/events/test_common.py
@@ -1,6 +1,6 @@
 from collections import namedtuple
 
-from django_log_formatter_asim.events.common import _get_client_ip_address
+from django_log_formatter_asim.events.common import Activity
 
 
 def test_get_client_ip_address_uses_X_FORWARDED_FOR():
@@ -10,4 +10,4 @@ def test_get_client_ip_address_uses_X_FORWARDED_FOR():
         },
     )
 
-    assert _get_client_ip_address(wsgi_request) == "90.243.238.50"
+    assert Activity()._get_client_ip_address(wsgi_request) == "90.243.238.50"
